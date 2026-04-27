5e8114a9f041db2fb1ff5eb02e5027a1664f1c21
jafari <@jafari>
2026-04-27 16:21:16 +0330

    Add IRepository

---
+++ PartnerPlan.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace PartnerPlan.Domain.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsybc(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
