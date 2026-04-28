7f7de75dac12ccde0732cdcd84680a5f10471532
jafari <@jafari>
2026-04-28 08:35:10 +0330

    Add Configurations folder and create user config

---
+++ PartnerPlan.Infrastructure/Configurations/UserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PartnerPlan.Domain.Entities;
+
+namespace PartnerPlan.Infrastructure.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.Email).HasMaxLength(256).IsRequired();
+            builder.Property(x => x.HashPassword).HasMaxLength(256);
+        }
+    }
+}
